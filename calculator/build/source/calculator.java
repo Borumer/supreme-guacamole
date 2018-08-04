a344221bc109e9d8b401429d42ab9e3d0cf807ad
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-08-04 16:13:36 -0400

    Processing Pacakge installed, build folder

--- calculator/build/source/calculator.java
+++ calculator/build/source/calculator.java
@@ -1,5 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.ArrayList; 
+import java.util.Arrays; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class calculator extends PApplet {
+
+
+
 
 public String result = "";
 float screenMargin = 40;
@@ -354,3 +373,12 @@ public String exponentOperation(ArrayList<String> arr) {
       }
     }
   }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "calculator" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}

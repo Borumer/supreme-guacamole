969fee05db225627ebb3d889831be9df0f6704ee
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-08-06 13:10:29 -0400

    Insert schoolcatalogue.js

--- Codecademy/apartment.js
+++ Codecademy/apartment.js
@@ -1,3 +1,39 @@
+class RentalUnit {
+  constructor(address, costPerYear) {
+    this._address = address;
+    this._costPerYear = costPerYear;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get costPerYear() {
+    return this._costPerYear;
+  }
+
+  calculateMonthly() {
+    return this.costPerYear / 12; 
+  }
+}
+
+class Apartment extends RentalUnit {
+  constructor(address, costPerYear, numberOfBedrooms) {
+    super(address, costPerYear);
+    this._numberOfBedrooms = numberOfBedrooms;
+  }
+
+  get numberOfBedrooms() {
+    return this._numberOfBedrooms;
+  }
+
+  static randomBorough() {
+    const myBoroughs = ['Manhattan', 'Brooklyn', 'Queens', 'The Bronx', 'Staten Island'];
+    return myBoroughs[Math.floor(Math.random() * 5)];
+  }
+}
+
+let myApartment = new Apartment('1234 W 54th', 22000, 3);
 function apartmentComplex() {
 	let address = '14th street, New York, NY';
 	const gym = "Inner-Section, Building 8";

81ee109b7d97fe45814284d11e2e7fc27869f299
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-08-14 16:32:30 -0400

    Insert message mixer project

---
+++ Codecademy/MessageMixer/src/message.js
@@ -0,0 +1,4 @@
+import {countCharacter, capitalizeFirstCharacterOfWords as titleCase, reverseWord, reverseAllWords, replaceFirstOccurence, encode, palindrome, pigLatin, displayMessage} from './messageMixer';
+
+displayMessage();
+console.log("hi");
